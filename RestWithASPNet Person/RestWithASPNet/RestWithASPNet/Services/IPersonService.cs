460afb82c8e32e76e62db384468a78afa2054656
Wellington Jr <wellingtonmm.jr@gmail.com>
2021-11-26 16:47:55 -0300

    Basic Model Object Class Person
    Implemented Controller and Class

---
+++ RestWithASPNet Person/RestWithASPNet/RestWithASPNet/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNet.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNet.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+    }
+}
